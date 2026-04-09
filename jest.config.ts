736c4d3c5c8c69987a114b0f51f93a4e64522416
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-04-09 16:30:12 -0500

    Update Jest Config

--- jest.config.ts
+++ jest.config.ts
@@ -171,12 +171,11 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['node_modules/(?!(\\oidc-client-ts)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(@fossa-app|@fable-org|oidc-client-ts)/)'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
